f83ba3b0d3f78f9caf1e593304a1d92960592cd3
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2019-12-17 15:33:56 -0300

    exercicio array for each

--- arrays/DesafioArray.java
+++ arrays/DesafioArray.java
@@ -1,5 +1,42 @@
 package arrays;
 
-public class DesafioArray {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class DesafioArray {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.printf("Digite a quantidade de notas que serão informadas: ");
+		int qtdNotas = entrada.nextInt();
+		
+		double[] notas = new double[qtdNotas];
+		
+		for(int i = 0; i < qtdNotas; i++) {
+			System.out.print("Digite a nota número " + (i + 1) + ": ");
+			double nota = entrada.nextDouble();
+			if(nota >= 0 && nota <= 10) {
+				notas[i] = nota;
+			}
+			else {
+				System.out.println("Nota invalida");
+				break;
+			}			
+		}
+		
+		System.out.println("As notas digitadas foram: " + Arrays.toString(notas));
+		
+		System.out.print("Deseja calcular a média? (S/N)");
+		String resposta = entrada.next();
+		
+		if (resposta.equalsIgnoreCase("S")) {
+			double total = 0;
+			for (double nota: notas) {
+				total += nota;
+			}
+			System.out.println("A média final do aluno é: " + total / notas.length);
+		}
+		
+		entrada.close();
+	}
 }
