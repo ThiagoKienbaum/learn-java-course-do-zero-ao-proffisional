ef9d08e5f2eb1f6333f11cc08013203b9efcc45b
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2019-12-21 14:33:26 -0300

    Map-HashMap

---
+++ colecoes/Mapa.java
@@ -0,0 +1,44 @@
+package colecoes;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Mapa {
+	public static void main (String[] args) {
+		Map<Integer, String> usuarios = new HashMap<>();
+		
+		usuarios.put(1, "Seya");
+		usuarios.put(2, "Shiryu");
+		usuarios.put(3, "Shun");
+		usuarios.put(4, "Hyoga");
+		usuarios.put(5, "Ikki");
+		
+		System.out.println(usuarios.size());
+		System.out.println(usuarios.isEmpty());
+		System.out.println(usuarios.get(3));
+		System.out.println(usuarios.keySet());
+		System.out.println(usuarios.values());
+		System.out.println(usuarios.entrySet());
+		System.out.println(usuarios.containsKey(1));
+		System.out.println(usuarios.containsValue("Ikki") + "\n");
+		
+		for(int chave: usuarios.keySet()) {
+			System.out.print("[" + chave + "]");
+		}
+		
+		System.out.println("\n");
+		
+		for(String valor: usuarios.values()) {
+			System.out.print("[" + valor + "]");			
+		}
+		
+		System.out.println("\n");
+		
+		for(Entry<Integer, String> usuario: usuarios.entrySet()) {
+			System.out.print("[" + usuario.getKey() + " - ");
+			System.out.print(usuario.getValue() + "]");
+			System.out.print("\n");
+		}
+	}
+}
